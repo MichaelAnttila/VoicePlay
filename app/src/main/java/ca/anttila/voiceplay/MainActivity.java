47727a34a78fd015f2d3c0d8c28d4bf96303bd17
Michael Anttila <michael@anttila.ca>
2015-02-21 22:20:37 -0500

    First commit of VoicePlay Android App.

---
+++ app/src/main/java/ca/anttila/voiceplay/MainActivity.java
@@ -0,0 +1,60 @@
+package ca.anttila.voiceplay;
+
+import android.os.StrictMode;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void submitCommand(View view) throws IOException {
+        String messageText = ((EditText)findViewById(R.id.editText)).getText().toString();
+        int server_port = 9930;
+        DatagramSocket s = new DatagramSocket();
+        InetAddress local = InetAddress.getByName("192.168.1.68");
+        int msg_length = messageText.length();
+        byte[] message = messageText.getBytes();
+        DatagramPacket p = new DatagramPacket(message, msg_length, local, server_port);
+        s.send(p);
+    }
+}
