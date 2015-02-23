76638e5900a6317a470d8714ce2043cd7e2f7303
Michael Anttila <michael@anttila.ca>
2015-02-23 02:20:15 +0000

    Refactored code to look nicer.

---
+++ Pi/log.h
@@ -0,0 +1,20 @@
+#ifndef LOG_H_INCLUDED
+#define LOG_H_INCLUDED
+
+namespace VoicePlay {
+
+class Log
+{
+public:
+	Log();
+	~Log();
+	static Log* Create();
+	void Info(char const* const format, ...);
+	void Error(char const* const format, ...);
+private:
+	void Write(char const* const format, va_list args);
+};
+
+}
+
+#endif
--- Pi/vps.cc
+++ Pi/vps.cc
@@ -1,54 +1,30 @@
-#include <arpa/inet.h>
-#include <netinet/in.h>
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <unistd.h>
 #include <string.h>
+#include <memory>
+#include "log.h"
+#include "udp.h"
 
 #define PORT 9930
 
-int min(int a, int b)
-{
-	if (a < b) {
-		return a;
-	} else {
-		return b;
-	}
-}
-
 int main(void)
 {
-	printf("VoicePlay\n");
+	std::shared_ptr<VoicePlay::Log> log = std::shared_ptr<VoicePlay::Log>(VoicePlay::Log::Create());
+	log->Info("VoicePlay");
 
-	int s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-	if (s == -1) {
-		printf("Call to socket() failed.\n");
-		return 1;
-	}
-
-	struct sockaddr_in si_me = {};
-	si_me.sin_family = AF_INET;
-	si_me.sin_port = htons(PORT);
-	si_me.sin_addr.s_addr = htonl(INADDR_ANY);
-	if (bind(s, reinterpret_cast<sockaddr*>(&si_me), sizeof(si_me)) == -1) {
-		printf("Call to bind() failed.\n");
+	std::shared_ptr<VoicePlay::UDP> udp = std::shared_ptr<VoicePlay::UDP>(VoicePlay::UDP::Create(log, PORT));
+	if (!udp) {
+		log->Error("Error: Could not create UDP object.");
 		return 1;
 	}
 
 	while (true) {
-		struct sockaddr_in si_other = {};
-		socklen_t slen = sizeof(si_other);
 		char buf[1024];
-		int bytesreceived = recvfrom(s, buf, sizeof(buf), 0, reinterpret_cast<sockaddr*>(&si_other), &slen);
-		if (bytesreceived == -1) {
-			printf("recvfrom()\n");
-			return 1;
+		if (!udp->Receive(buf, sizeof(buf))) {
+			log->Error("Error: Encountered an error while trying to read data from the network.");
+			return 2;
 		}
-		buf[min(bytesreceived, sizeof(buf)-1)] = 0;
-		printf("Received packet from %s:%d\nData: %s\nSize: %d\n\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port), buf, bytesreceived);
+		log->Info("Received packet: %s", buf);
 	}
 
-	close(s);
 	return 0;
 }
